76bc82e645d56df804046e79f82ad7a1b8bcc0c3
Tevin-creator <isaactevin67@gmail..com>
2021-07-20 00:01:02 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteComponent } from './quote/quote.component';
+import { DatesComponent } from './dates/dates.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent,
+    QuoteComponent,
+    DatesComponent
   ],
   imports: [
     BrowserModule,
