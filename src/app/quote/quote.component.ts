9b75225ed1e728ecf907cb4d2bfdf65717661672
Tevin-creator <isaactevin67@gmail..com>
2021-07-20 01:46:46 +0300

    added buttons in quote details

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+
 
 @Component({
   selector: 'app-quote',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes = [
 
-  constructor() { }
+    new Quote(1, "Indira Gandhi", "You cannot shake hands with a clenched fist", new Date(2016.11, 15)),
 
-  ngOnInit(): void {
+    
+   
+  ]
+  deleteQuote(isComplete,index){
+    if (isComplete){
+      let toDelete=confirm('Are you sure you want to delete quote')
+      if(toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
   }
 
+    addNewQuote(quote){
+        let quoteLength = this.quotes.length;
+        quote.id=quoteLength+1;
+        quote.completeDate = new Date(quote.completeDate)
+        this.quotes.push(quote)
+
+    }
+
+    upVotes:number=0;
+    upVote(){
+      this.upVotes=this.upVotes+1;
+
+    }
+    downVotes:number=0;
+    downVote(){
+      this.downVotes=this.downVotes+1;
+
+    }
+    constructor() { }
+
+    ngOnInit() {
+    }
 }
